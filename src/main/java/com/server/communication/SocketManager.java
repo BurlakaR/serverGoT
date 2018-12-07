7ed4068d0d448d1c173be686a4efc8f3e547b15e
BurlakaR <bugaga1998@gmail.com>
2018-12-07 10:13:38 +0200

    paintTheMap.synchro

--- src/main/java/com/server/communication/SocketManager.java
+++ src/main/java/com/server/communication/SocketManager.java
@@ -2,6 +2,7 @@ package com.communication;
 
 import com.common.IntegerMessage;
 import com.common.Message;
+import com.common.SocketManagerCommon;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +13,7 @@ import java.net.Socket;
 import java.util.*;
 
 @Component
-public class SocketManager {
+public class SocketManager extends SocketManagerCommon{
     static int portCreation=9998;
     static int portConnection=9999;
     ServerSocket serverCreation;
